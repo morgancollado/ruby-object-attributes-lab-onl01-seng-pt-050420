4da2e5d637b3f5bdc9a6bc0f5285adf7b7d54e22
Morgan Collado <morgan.collado@gmail.com>
2020-05-18 20:37:43 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,17 @@
 class Dog
   def name=(dogs_name)
-    
+    @dogs_name = dogs_name
+  end 
+  
+  def name 
+    @dogs_name
+  end 
+  
+  def breed=(dog_breed)
+    @dog_breed = dog_breed
+  end
+  
+  def breed 
+    @dog_breed
+  end 
 end 
